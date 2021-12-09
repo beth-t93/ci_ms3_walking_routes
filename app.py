2f266ac2665e7a8edcb3cd373d4f1c735ad5369f
Beth Travis <beth_t93@live.co.uk>
2021-12-09 22:43:34 +0000

    fix issue with register and add login function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 import os
+import bcrypt
 
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
@@ -8,6 +9,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from os import environ, path
 from dotenv import load_dotenv
 from pathlib import Path
+
+
 
 env_path = Path(".env")
 load_dotenv(dotenv_path=env_path)
@@ -30,24 +33,59 @@ def trails():
     return render_template("index.html", trails=trails)
 
 
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('trails'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+'''
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('trails'))
+
+    return 'Invalid username/password combination'
+'''
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
     if request.method == "POST":
-        existing_user = mongo.db.find_one({"username": request.form.get("username")})
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
 
         if existing_user:
-            flash("Username already exisits")
-            return redirect(url_for("signup"))
-        
-        signup = {
-            "username": request.form.get("username"),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(signup)
+            # ensure hashed password matches user input
+            if bcrypt.hashpw(request.form['pass'].encode('utf-8'), existing_user['password'].encode('utf-8')) == existing_user['password'].encode('utf-8'):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
 
-        session["user"] = request.form.get("username")
-        flash("You have succesfully signed up!")
-    return render_template("signup.html")
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("myaccount.html")
 
 
 if __name__ == "__main__":
